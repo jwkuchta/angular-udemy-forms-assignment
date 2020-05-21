e76aaa2eca32304b33329a1a0d9ad77c26b02746
Justina Kuchta <justyna.kuchta@gmail.com>
2020-05-21 19:53:23 -0400

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  defaultType = "advanced"
+  formSubmitted = false
+  userData = {
+    email: '',
+    subscription: '',
+    password: ''
+  }
+
+  onSubmit(form) {
+    console.log(form)
+    this.formSubmitted = true
+    this.userData.email = form.value.email
+    this.userData.subscription = form.value.subscription
+    this.userData.password = form.value.password
+    form.reset()
+  }
 
 }
